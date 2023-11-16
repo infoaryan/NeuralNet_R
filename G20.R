25c63a7f7cb05b262a92bce115d08bef966153c6
Chloe <chloestip@gmail.com>
2023-11-16 17:03:40 +0000

    Start updating comments

--- G20.R
+++ G20.R
@@ -1,5 +1,13 @@
+# INPUT:      d, a vector of numbers giving the number of nodes in
+#             each layer of a nn.
+# OUTPUT:     nn, a neural network with nodes, h, weights, W, and 
+#             bias values b. 
+# PURPOSE:    Construct and initialize a nn with nodes, h, weights, W, and
+#             bias values b. The nodes are initialized to 0 and the W and b 
+#             values are initialized with elements drawn from a Uniform(0, 0.2).
 netup <- function(d) {
-  # Check if the d has at least 2 elements 
+  
+  # Check that the nn has at least 1 hidden layer
   if (length(d) < 2) {
     stop("A neural network should have at least 1 hidden layer.")
   }
@@ -8,88 +16,98 @@ netup <- function(d) {
   nn <- list()
   
   # Initialize the lists for nodes, weights, and bias
-  h <- list()
-  W <- list()
-  b <- list()
+  h <- list(); W <- list(); b <- list()
   
-  set.seed(111) 
-  
-  # Loop through each layer dim and initialize the values
+  # For each layer of the nn:
+  #   Initialize the values of the nodes, h, to 0
+  #   Initialize the values of the weights, W, and biases, b, to be
+  #   Uniform(0, 0.2)
   for (l in 1:(length(d))) {
-    # Initialize nodes for this layer that will contain node values
-    h[[l]] <- numeric(d[l])
     
+    h[[l]] <- numeric(d[l]) # Initialize h
     if(l != length(d)){
-      # Initialize weight matrix W[[l]] with random values from U(0, 0.2)
-      W[[l]] <- matrix(runif(d[l + 1] * d[l], 0, 0.2), nrow = d[l+1], ncol = d[l])
-      
-      # Initialize offset vector b[[l]] with random values from U(0, 0.2)
-      b[[l]] <- matrix(runif(d[l + 1], 0, 0.2), nrow = d[l+1])
+      W[[l]] <- matrix(runif(d[l + 1] * d[l], 0, 0.2), # Initialize W
+                       nrow = d[l+1], ncol = d[l])
+      b[[l]] <- matrix(runif(d[l + 1], 0, 0.2), # Initialize b
+                       nrow = d[l+1])
     }
   }
   
-  nn$h <- h
-  nn$W <- W
-  nn$b <- b
+  nn$h <- h; nn$W <- W; nn$b <- b
   
-  return(nn)
+  return(nn) 
 }
 
+# INPUT:      nn, a nn defined by netup.
+#             inp, input values for a particular data point (1x4).
+# OUTPUT:     nn, the neural network after forward propagation, i.e. with
+#             updated node values according to the W and b defined in nn.
+# PURPOSE:    Given a nn with defined W and b, and a set of input values,
+#             this function will calculate the values of the nodes at each
+#             layer of the nn, applying a ReLU activation function:
+#             h^{l+1} = max(0, W^l %*% h^l + b^l)
 forward <- function(nn, inp) {
-  h <- nn$h; W <- nn$W; b <- nn$b
-  inp <- matrix(inp[1,], nrow = 1)
-  h[[1]] <- t(inp)
+  
+  h <- nn$h; W <- nn$W; b <- nn$b 
+  inp <- matrix(inp[1,], nrow = 1) # ensure inp is the correct form
+  h[[1]] <- t(inp) # feed inp values into first layer of nn
   for(i in 1:(length(h)-1)){
-    h[[i+1]] <- apply(W[[i]]%*%h[[i]], 2,
-                      function(x){ x + b[[i]] })
+    h[[i+1]] <- apply(W[[i]]%*%h[[i]], 2, 
+                      function(x){ x + b[[i]] }) # Calculate h^{l+1} = W^l %*% h^l + b^l
     h[[i+1]] <- apply(h[[i+1]], 1:2,
-                      function(x){ max(0,x) })
+                      function(x){ max(0,x) }) # Apply the ReLU transformation
   }
   nn$h <- h
   return(nn)
 }
 
-
-# Function to perform backward pass and update weights and biases
-
-backward <- function(nn_forward, k_forward) {
+# INPUT:      nn, a nn which has been passed through forward.
+#             k, the class values corresponding to the inputs used to define
+#             nn.
+# OUTPUT:     nn, the same nn which was given as an input but with added
+#             elements, dh, dW and db the derivative of the Loss Function wrt
+#             the nodes, Weights and biases respectively.
+# PURPOSE:    To perform back-propagation by calculating the derivative of the Loss
+#             Function wrt the nodes, Weights, and biases.
+backward <- function(nn, k) {
   
-  h <- nn_forward$h; W <- nn_forward$W; b <- nn_forward$b
-  L <- length(h); L
-  k <- k_forward[,1]
+  h <- nn$h; W <- nn$W; b <- nn$b
+  L <- length(h); L # define the number of layers in nn
+  k <- k[,1] # ensure k has the correct dimensions
 
   # initialize derivatives
-  dh <- vector("list", length = L); dh
-  dW <- vector("list", length = L-1); dW
-  db <- vector("list", length = L-1); db
+  dh <- vector("list", length = L)
+  dW <- vector("list", length = L-1)
+  db <- vector("list", length = L-1)
   
   for (l in L:1){
-    if (l == L){
-      # i = 3
-      #dh_L <- matrix()
-      dh_L <- exp(h[[l]]) / colSums(exp(h[[l]])); dh_L
+    if (l == L){ # final layer
+      # Compute the derivative of the loss wrt the final layer
+      dh_L <- exp(h[[l]]) / colSums(exp(h[[l]])) 
       dh_L[k,] <-  dh_L[k,] - 1; dh_L
       dh[[l]] <- dh_L
       
     }
     else{
-      d_l_plus_1 <- dh[[l+1]]
-      d_l_plus_1[h[[l+1]] < 0] <- 0
+      d_l_plus_1 <- dh[[l+1]] # derivative of the loss wrt h^{l+1}
+      d_l_plus_1[h[[l+1]] < 0] <- 0 # set dh values to 0 where h values are <0
       #d_l_plus_1 <- apply(dh[[l+1]], 1:2,
       #                    function(x){ max(0,x) })
-      dh[[l]] <- t(W[[l]]) %*% d_l_plus_1; dh
-      db[[l]] <- d_l_plus_1; db
-      dW[[l]] <- d_l_plus_1 %*% t(h[[l]]); dW
+      dh[[l]] <- t(W[[l]]) %*% d_l_plus_1; dh # derivative of loss wrt h
+      db[[l]] <- d_l_plus_1; db # derivative of loss wrt b
+      dW[[l]] <- d_l_plus_1 %*% t(h[[l]]); dW # derivative of loss wrt W
     }
   }
   
-  nn_forward$dh <- dh
-  nn_forward$dW <- dW
-  nn_forward$db <- db
-  return(nn_forward)
-  
+  nn$dh <- dh
+  nn$dW <- dW
+  nn$db <- db
+  return(nn)
 }
 
+# INPUT:      
+# OUTPUT:     
+# PURPOSE:    
 # Function which performs matrix summation in a list of matrices
 sum_lists_elementwise <- function(list1, list2) {
   
@@ -110,10 +128,12 @@ subtract_lists_elementwise <- function(list1, list2) {
   return(result_list)
 }
 
+# INPUT:      
+# OUTPUT:     
+# PURPOSE:    
 # Training function
 train <- function(nn, inp, k, eta = 0.01, subset_size = 10, nstep = 10000) {
 
-  set.seed(2)
   loss_values <- c()
   loss <- 0
   
@@ -168,12 +188,16 @@ train <- function(nn, inp, k, eta = 0.01, subset_size = 10, nstep = 10000) {
     if (step %% 1000 == 0) {
      cat("Step:", step, "  Loss:", loss, "\n")
     }
+    loss_values <- c(loss_values, loss)
   }
   
   return(nn)
 }
 
-# Function generatiung a confusion matrix
+# INPUT:      
+# OUTPUT:     
+# PURPOSE:    
+# Function generating a confusion matrix
 cnf_generator <- function(k_pred, k_actual){
   confusion_matrix <- matrix(0, nrow=3, ncol=3)
   sample_size <- length(k_pred)
@@ -197,7 +221,9 @@ test_indices <- seq(5, nrow(iris), by = 5)
 inp_training <- inp[-test_indices, ]; k_training <- k[-test_indices, ]
 inp_testing <- inp[test_indices, ]; k_testing <- k[test_indices, ]
 
-# Construct Neural Net
+# Set the seed for pseudo-random number generation
+set.seed(4)
+
 nn <- netup(c(4,8,7,3))
 
 # Train Neural Net on training data
